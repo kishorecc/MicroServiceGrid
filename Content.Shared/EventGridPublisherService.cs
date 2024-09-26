7cab2960a9b7d4162974f273c5d2494b3e61d6ef
kishorecc <47027765+kishorecc@users.noreply.github.com>
2024-09-26 14:55:37 +0530

    initial push

---
+++ Content.Shared/EventGridPublisherService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventGrid;
+using Microsoft.Azure.EventGrid.Models;
+
+namespace ContentReactor.Shared
+{
+    public interface IEventGridPublisherService
+    {
+        Task PostEventGridEventAsync<T>(string type, string subject, T payload);
+    }
+
+    public class EventGridPublisherService : IEventGridPublisherService
+    {
+        public Task PostEventGridEventAsync<T>(string type, string subject, T payload)
+        {
+            // get the connection details for the Event Grid topic
+            var topicEndpointUri = new Uri("https://contenttopic.southindia-1.eventgrid.azure.net/api/events");
+            var topicEndpointHostname = topicEndpointUri.Host;
+            var topicKey ="1CnQFVxUVTUUEuUu80gQ0jBWh2Cs2aTCXWyCzvUSK7zyD8DjpXtEJQQJ99AIAC77bzfXJ3w3AAABAZEG7usV";
+            var topicCredentials = new TopicCredentials(topicKey);
+
+            // prepare the events for submission to Event Grid
+            var events = new List<Microsoft.Azure.EventGrid.Models.EventGridEvent>
+            {
+                new Microsoft.Azure.EventGrid.Models.EventGridEvent
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    EventType = type,
+                    Subject = subject,
+                    EventTime = DateTime.UtcNow,
+                    Data = payload,
+                    DataVersion = "1"
+                }
+            };
+
+            // publish the events
+            var client = new EventGridClient(topicCredentials);
+            return client.PublishEventsWithHttpMessagesAsync(topicEndpointHostname, events);
+        }
+    }
+}
---
+++ Content.Text/Content.Text.Service/Repositories/TextRepository.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using ContentReactor.Shared;
+using ContentReactor.Text.Services.Models.Data;
+using ContentReactor.Text.Services.Models.Responses;
+using ContentReactor.Text.Services.Models.Results;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+
+namespace ContentReactor.Text.Services.Repositories
+{
+    public interface ITextRepository
+    {
+        Task<string> AddTextNoteAsync(TextDocument textObject);
+        Task<DeleteTextNoteResult> DeleteTextNoteAsync(string textId, string userId);
+        Task UpdateTextNoteAsync(TextDocument textDocument);
+        Task<TextDocument> GetTextNoteAsync(string textId, string userId);
+        Task<IList<TextNoteSummary>> ListTextNotesAsync(string userId);
+    }
+
+    public class TextRepository : ITextRepository
+    {
+        private static readonly string EndpointUrl = "https://dddcqrs-cosmos.documents.azure.com:443/";
+        private static readonly string AccountKey = "yFoQzJyfC6Swwxiyl75dy0XQunXFCmLvRMInQTh9HOtNDLdPygxHsAchwqR18SF0W74lZiTBFOZiACDb4Oi25A==";
+        private static readonly string DatabaseName = "CQRS";
+        private static readonly string CollectionName = "Text";
+        private static  DocumentClient DocumentClient; //= new DocumentClient(new Uri(EndpointUrl), AccountKey);
+        
+        private const int MaximumTextPreviewLength = 100;
+
+        public async Task<string> AddTextNoteAsync(TextDocument textDocument)
+        {
+            DocumentClient = new DocumentClient(new Uri(EndpointUrl), AccountKey);
+            var documentUri = UriFactory.CreateDocumentCollectionUri(DatabaseName, CollectionName);
+            Document doc = await DocumentClient.CreateDocumentAsync(documentUri, textDocument);
+            return doc.Id;
+        }
+
+        public async Task<DeleteTextNoteResult> DeleteTextNoteAsync(string textId, string userId)
+        {
+            DocumentClient = new DocumentClient(new Uri(EndpointUrl), AccountKey);
+            var documentUri = UriFactory.CreateDocumentUri(DatabaseName, CollectionName, textId);
+            try
+            {
+                await DocumentClient.DeleteDocumentAsync(documentUri, new RequestOptions { PartitionKey = new PartitionKey(userId)});
+                return DeleteTextNoteResult.Success;
+            }
+            catch (DocumentClientException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                // we return the NotFound result to indicate the document was not found
+                return DeleteTextNoteResult.NotFound;
+            }
+        }
+
+        public Task UpdateTextNoteAsync(TextDocument textDocument)
+        {
+            DocumentClient = new DocumentClient(new Uri(EndpointUrl), AccountKey);
+            var documentUri = UriFactory.CreateDocumentUri(DatabaseName, CollectionName, textDocument.Id);
+            return DocumentClient.ReplaceDocumentAsync(documentUri, textDocument);
+        }
+        
+        public async Task<TextDocument> GetTextNoteAsync(string textId, string userId)
+        {
+            DocumentClient = new DocumentClient(new Uri(EndpointUrl), AccountKey);
+            var documentUri = UriFactory.CreateDocumentUri(DatabaseName, CollectionName, textId);
+            try
+            {
+                var documentResponse = await DocumentClient.ReadDocumentAsync<TextDocument>(documentUri, new RequestOptions { PartitionKey = new PartitionKey(userId) });
+                return documentResponse.Document;
+            }
+            catch (DocumentClientException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                // we return null to indicate the document was not found
+                return null;
+            }
+        }
+
+        public async Task<IList<TextNoteSummary>> ListTextNotesAsync(string userId)
+        {
+            DocumentClient = new DocumentClient(new Uri(EndpointUrl), AccountKey);
+            var documentUri = UriFactory.CreateDocumentCollectionUri(DatabaseName, CollectionName);
+
+            // create a query to just get the document ids
+            var query = DocumentClient
+                .CreateDocumentQuery<TextDocument>(documentUri)
+                .Where(d => d.UserId == userId)
+                .Select(d => new TextNoteSummary { Id = d.Id, Preview = d.Text } )
+                .AsDocumentQuery();
+            
+            // iterate until we have all of the ids
+            var list = new List<TextNoteSummary>();
+            while (query.HasMoreResults)
+            {
+                var ids = await query.ExecuteNextAsync<TextNoteSummary>();
+                list.AddRange(ids.Select(d => new TextNoteSummary { Id = d.Id, Preview = d.Preview.Truncate(MaximumTextPreviewLength) }));
+            }
+            return list;
+        }
+    }
+}
